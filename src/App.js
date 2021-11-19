18946bf7de49016b399ac6b64ba3672e6b2a65d2
Giuliano Machado <giufma@gmail.com>
2021-11-18 21:14:50 -0300

    add hacker news fetch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import './App.css';
-import UseStateApp from './components/useState';
-import UseEffectApp from './components/useEffect';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState({ hits: [] });
+
+  useEffect(async () => {
+    const result = await fetch('https://hn.algolia.com/api/v1/search?query=redux');
+    const resultJson = await result.json();
+    console.log(resultJson);
+    setData(resultJson);
+  }, []);
+
   return (
-    <div>
-      <UseStateApp />
-      <UseEffectApp />
-    </div>
+    <ul>
+      {data.hits.map(item => {
+        <li key={item.objectId}>
+          <a href={item.url}>{item.title}</a>
+        </li>
+      })}
+    </ul>
   )
 }
 
---
+++ src/StateEffectHooksDemo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import UseStateApp from './components/useState';
+import UseEffectApp from './components/useEffect';
+
+function App() {
+  return (
+    <div>
+      <UseStateApp />
+      <UseEffectApp />
+    </div>
+  )
+}
+
+export default App;
